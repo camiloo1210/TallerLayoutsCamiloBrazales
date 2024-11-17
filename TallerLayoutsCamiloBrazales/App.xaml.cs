19a933e47c2388a1641f0a79b67bae4acd8bca37
SejoLape123 <camilobrazales@gmail.com>
2024-11-17 10:55:36 -0500

    Agregar archivos de proyecto.

---
+++ TallerLayoutsCamiloBrazales/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerLayoutsCamiloBrazales
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ TallerLayoutsCamiloBrazales/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TallerLayoutsCamiloBrazales
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
