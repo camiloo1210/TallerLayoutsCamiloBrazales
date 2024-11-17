b82e7376da9e22330920dc983356102389cb92f6
SejoLape123 <camilobrazales@gmail.com>
2024-11-17 11:39:09 -0500

    Creacion de Grid con 4 colores y boton que lleva a la segunda pagina ya creada

--- TallerLayoutsCamiloBrazales/Views/GridColor.xaml.cs
+++ TallerLayoutsCamiloBrazales/Views/GridColor.xaml.cs
@@ -6,4 +6,8 @@ public partial class GridColor : ContentPage
 	{
 		InitializeComponent();
 	}
+	private void Boton1(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new Page2());
+	}
 }
---
+++ TallerLayoutsCamiloBrazales/Views/Page2.xaml.cs
@@ -0,0 +1,9 @@
+namespace TallerLayoutsCamiloBrazales.Views;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+}
